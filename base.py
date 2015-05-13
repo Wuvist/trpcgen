9830e1f7ee41b07a9e787861c1ddfb12a04b3fa8
Wuvist <wuvist@gmail.com>
2015-05-13 13:52:40 +0800

    Add services handling

--- base.py
+++ base.py
@@ -14,13 +14,39 @@ namespace = ""
 thrift_file = ""
 
 
-def init_Fields(obj):
-	idField = obj.fields[0]
-	obj.idField = idField
+def init_module(module):
+	module.consts = []
+	module.typedef = []
+	module.enums = []
+	module.structs = []
+	module.services = []
 
-	obj.relateObj = {}
-	obj.fieldMap = {}
+	for node in [i for i in module.values() if isinstance(i, ast.Typedef)]:
+		node.go_type = type_translate(node.type)
+		module.typedefs.append(node)
+		typedef[str(node.name)] = node.type
 
+	for node in module.values():
+		if not isinstance(node, ast.Node):
+			continue
+		if isinstance(node, ast.Enum):
+			module.enums.append(node)
+		elif isinstance(node, ast.Const):
+			module.consts.append(transform_const(node))
+		elif isinstance(node, ast.Struct):
+			module.structs.append(node)
+		elif isinstance(node, ast.Service):
+			module.services.append(node)
+
+	## process enum labels
+	for obj in module.enums:
+		obj.labels = {}
+		for i in obj.values:
+			label_anno = [j.value for j in i.annotations if j.name.value == "label"]
+			if len(label_anno) == 0:
+				obj.labels[i.tag] = '"%s"' % i.name
+			else:
+				obj.labels[i.tag] = label_anno[0]
 
 def load_thrift(thrift_idl):
 	global thrift_file, namespace
@@ -33,6 +59,9 @@ def load_thrift(thrift_idl):
 
 		loader.namespace = str(loader.namespace)
 		namespace = loader.namespace
+
+		for module in loader.modules.values():
+			init_module(module)
 	except Exception, e:
 		print "error", thrift_file
 		traceback.print_exc()
